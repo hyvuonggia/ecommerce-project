26a317217e1812b64bb33d6ac5babaa8d32137cc
Vuong Gia Hy <hyvuonggia@gmail.com>
2024-04-21 19:43:31 +0700

    Add list product

--- 02-backend/spring-boot-ecommerce/src/main/java/com/hyvuonggia/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/hyvuonggia/ecommerce/dao/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.hyvuonggia.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.hyvuonggia.ecommerce.entity.Product;
 
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
